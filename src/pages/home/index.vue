c99bd40c50f4439bb9e1785a3eaacacb6777961a
sunhui44311 <1874314758@qq.com>
2020-02-03 17:24:48 +0800

    updata

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,33 +1,28 @@
 <template>
   <div id="home">
-   <van-list
-  v-model="loading"
-  :finished="finished"
-  finished-text="没有更多了"
-  @load="loadMore()"
-  :immediate-check='false'
-  class="list"
->
-      <div
-        class="item"
-        @click="tiaozhuan(item)"
-        v-for="(item,index) in list"
-        :key="index"
-      >
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="loadMore()"
+      :immediate-check="false"
+      class="list"
+    >
+      <div class="item" @click="tiaozhuan(item)" v-for="(item,index) in list" :key="index">
         <div class="left">
           <img :src="$getUrl(item.logo)" class="img" alt />
         </div>
         <div class="center">
           <div class="center-title">{{item.name}}</div>
           <div class="center-info">{{item.info}}</div>
-          <div class="center-tag">
+          <div class="center-tag" v-if="item.label">
             <van-tag class="tag" v-for="(item,index) in item.label" :key="index">{{item}}</van-tag>
           </div>
         </div>
         <div class="right">{{item.distance}}米</div>
       </div>
-</van-list>
-   <van-divider v-if="show">定位失败</van-divider>
+    </van-list>
+    <van-divider v-if="show">定位失败</van-divider>
   </div>
 </template>
 <script src="./index.js"></script>
